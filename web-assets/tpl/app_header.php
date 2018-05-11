29587417fdcdb796f07664ec37779a1dd901badc
Eddie Gichuhi <gichedw@gmail.com>
2018-05-11 03:02:33 -0400

    Restyle the spacing in the global header file..

--- web-assets/tpl/app_header.php
+++ web-assets/tpl/app_header.php
@@ -1,21 +1,26 @@
-<?php
-  session_start();
-  include_once $_SERVER['DOCUMENT_ROOT'] . '/never-note/lib/db.php';
-?> 
-
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title><?php echo $page_title ?></title>
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="/web-assets/css/bootstrap.min.css">
-
-  <title><?php echo $page_title ?></title>
+  <style>
+    html {
+      position: relative;
+      min-height: 100%;
+    }
+    body {
+      padding-top: 50px;
+      margin-bottom: 80px;
+    }
+  </style>
 </head>
 <body>
   <?php include_once $_SERVER['DOCUMENT_ROOT'] . "/web-assets/tpl/app_nav.php"; ?>
-  <div class="container">
-    <br>
+  <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/lib/db.php'; ?>
+  <div class="container-fluid">
