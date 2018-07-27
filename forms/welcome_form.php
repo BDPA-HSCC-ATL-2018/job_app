3631dde3ada6631d52359d0c3b7feefd8637228f
Christopher <christopherballenger@gmail.com>
2018-07-27 16:45:13 -0400

    It imports the geader now if it wasn't included from index.php so it can have the Bootstrap and other things it needs. An unnecessary value attribute was on that input tag for email, so that was removed.

--- forms/welcome_form.php
+++ forms/welcome_form.php
@@ -1,3 +1,8 @@
+<?php $page_title = "Welcome";
+  if (!isset($dbh)) {
+    include $_SERVER['DOCUMENT_ROOT'] . '/job_app/web-assets/tpl/app_header.php';
+  }
+?>
 <h1><?php echo $page_title ?></h1>
 
 <form id="welcome" method="post" action="/job_app/" class="needs-validation" novalidate>
@@ -9,7 +14,7 @@
       <div class="form-group">
         <label for="email_id" class="col-sm-2 col-form-label">Email</label>
         <div class= "col-md-6">
-          <input type="email" class="form-control" id="email_id" name="email_id" value="<?php echo $email_id; ?>" required>
+          <input type="email" class="form-control" id="email_id" name="email_id" required>
         </div>
       </div>
 
