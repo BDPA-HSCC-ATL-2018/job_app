3c8a6895934113f706e89d1aa952886caa6bc5e9
Christopher <christopherballenger@gmail.com>
2018-07-27 00:32:42 -0400

    The 'job-app' button now goes to welcome.php.

--- web-assets/tpl/app_nav.php
+++ web-assets/tpl/app_nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/job_app/">Job App</a>
+  <a class="navbar-brand" href="/job_app/index.php?action=welcome">Job App</a>
   <a href="/job_app/index.php?action=logout" class="btn btn-primary">Logout</a>
 </nav>
 <div class="container">
