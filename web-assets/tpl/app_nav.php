76f280030b6416671676ae1b9b9faf664367dce4
Christopher <christopherballenger@gmail.com>
2018-07-27 16:48:17 -0400

    The logout button only shows when the user is logged in now.

--- web-assets/tpl/app_nav.php
+++ web-assets/tpl/app_nav.php
@@ -1,6 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/job_app/index.php?action=welcome">Job App</a>
-  <a href="/job_app/index.php?action=logout" class="btn btn-primary">Logout</a>
+  <a class="navbar-brand" href="/job_app/welcome.php">Job App</a>
+  <?php
+    if (isset($_SESSION['email_id'])) {
+      echo "<a href='/job_app/index.php?action=logout' class='btn btn-primary'>Logout</a>";
+    }
+  ?>
 </nav>
 <div class="container">
 <br>
